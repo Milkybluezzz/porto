a44e922900c8595c93ec9fce6c83c515b9afa6fd
Milkybluezzz <radithiaannafi77@gmail.com>
2025-03-07 11:00:21 +0700

    Initial commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,240 @@
+<script>
+export default {
+  data() {
+    return {
+      activeTab: 1,
+      tech: [
+        {
+          id: 1,
+          name: 'HTML',
+          imageUrl: 'https://cdn-icons-png.flaticon.com/512/1051/1051277.png',
+          status: 'Beginner'
+        },
+        {
+          id: 2,
+          name: 'CSS',
+          imageUrl: 'https://cdn-icons-png.flaticon.com/512/732/732190.png',
+          status: 'Beginner'
+        },
+        {
+          id: 3,
+          name: 'Javascript',
+          imageUrl: 'https://cdn.jsdelivr.net/npm/programming-languages-logos@0.0.3/src/javascript/javascript.svg',
+          status: 'Beginner'
+        },
+        {
+          id: 4,
+          name: 'VueJS',
+          imageUrl: 'https://cdn.iconscout.com/icon/free/png-256/free-vue-282497.png?f=webp',
+          status: 'Beginner'
+        },
+        {
+          id: 5,
+          name: 'NodeJS',
+          imageUrl: 'https://cdn.iconscout.com/icon/free/png-256/free-node-js-1174925.png',
+          status: 'Beginner'
+        },
+        {
+          id: 6,
+          name: 'Tailwind',
+          imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg',
+          status: 'Beginner'
+        },
+        {
+          id: 7,
+          name: 'Laravel',
+          imageUrl: 'https://img.icons8.com/?size=96&id=lRjcvhvtR81o&format=png',
+          status: 'Beginner'
+        },
+        {
+          id: 8,
+          name: 'Canva',
+          imageUrl: 'https://www.canva.com/favicon.ico',
+          status: 'Advanced'
+        },
+        {
+          id: 9,
+          name: 'Figma',
+          imageUrl: 'https://cdn-icons-png.flaticon.com/128/5968/5968705.png',
+          status: 'Advanced'
+        }
+      ],
+      tools: [
+        {
+          id: 1,
+          name: 'Git',
+          imageUrl: 'https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png',
+          status: 'Version Control'
+        },
+        {
+          id: 2,
+          name: 'GitHub',
+          imageUrl: 'https://icon-library.com/images/github-icon-white/github-icon-white-6.jpg',
+          status: 'Git Hosting'
+        },
+        {
+          id: 3,
+          name: 'NPM',
+          imageUrl: 'https://cdn.iconscout.com/icon/free/png-256/free-npm-3-1175132.png',
+          status: 'Package Manager'
+        },
+        {
+          id: 4,
+          name: 'Vercel',
+          imageUrl: 'https://vercel.com/favicon.ico',
+          status: 'Deployment'
+        },
+      ]
+    };
+  }
+}
+</script>
+<template>
+  <div
+    class="bg-[#1e1e1f] px-5 py-5 md:px-12 md:py-10 text-left border border-[#383838] rounded-3xl text-blue-50 mx-3 mb-5">
+    <article data-page="about">
+
+      <header>
+        <div class="text-2xl font-bold text-white mb-5 fadein-bot title-section flex items-center">
+          About Me &nbsp;
+          <div class="h-[1px] w-32 bg-blue-200 md:w-96 aos-init aos-animate" data-aos="zoom-in-left"
+            data-aos-duration="600"></div>
+        </div>
+      </header>
+
+      <section
+        class="text-sm md:text-lg text-justify flex flex-col gap-4 md:flex-row md:gap-8 md:justify-left md:items-center">
+        <div class="flex justify-center">
+          <img class="w-9/12 rounded-full mb-3 fadein-up"
+            src="/img/profile2.jpeg" alt="Foto">
+        </div>
+        <div class="md:w-7/12">
+          <p class="mb-3 md:mb-7 fadein-left fadeins-1">
+            &nbsp; &nbsp; &nbsp;    Hai semuanya! Nama saya Radithia Annafi. Saya adalah seorang Pengembang Web Front-End Pemula dari Bandung, Indonesia.
+            Dengan pengalaman dua tahun dalam pengembangan situs web di Sekolah Menegah Kejuruan Negri 4 Bandung. Saya sangat menikmati apa yang saya lakukan. 
+            Menurut saya, membuat program bukan hanya sekedar pekerjaan tapi juga sebuah seni yang memiliki nilai estetika.
+          </p>
+          <p class="mb-3 fadein-left fadeins-2">
+            &nbsp; &nbsp; &nbsp;   Membangun situs web yang fungsional, mudah digunakan, dan menarik secara visual lebih dari sekadar pekerjaan bagi saya - ini adalah hobi dan gairah saya. 
+            Saya senang menambahkan sentuhan pribadi pada setiap proyek untuk memastikan bahwa setiap situs web tidak hanya menonjol tetapi juga menawarkan pengalaman pengguna yang mudah. 
+            Saya bersemangat untuk terus memberikan yang terbaik kepada klien di masa depan, mewujudkan visi mereka dengan cara yang paling kreatif dan efektif.
+          </p>
+        </div>
+
+      </section>
+
+    </article>
+  </div>
+
+  <div class="px-5 py-5 md:px-12 md:py-10 text-left text-blue-50 mx-3">
+    <article data-page="about">
+      <header>
+        <div class="text-2xl font-bold text-white mb-5 fadein-bot title-section flex items-center">
+          <div class="h-[1px] w-10 bg-blue-200 md:w-20 aos-init aos-animate" data-aos="zoom-in-left"
+            data-aos-duration="600"></div>
+          &nbsp; Tech & Skills
+        </div>
+      </header>
+      <section>
+        <div>
+          <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 dark:text-gray-400 mb-5">
+            <li class="mr-2">
+              <button class="inline-block px-4 py-3 rounded-lg hover:text-white"
+                :class="{ 'text-blue-200 bg-blue-200 bg-opacity-10': activeTab === 1 }" @click="activeTab = 1">Tech Stack</button>
+            </li>
+            <li class="mr-2">
+              <button class="inline-block px-4 py-3 rounded-lg hover:text-white"
+                :class="{ 'text-blue-200 bg-blue-200 bg-opacity-10': activeTab === 2 }" @click="activeTab = 2">Tools</button>
+            </li>
+          </ul>
+        </div>
+        <div v-show="activeTab === 1">
+          <div class="grid grid-cols-2 gap-4 pb-32 md:grid-cols-3 md:gap-8 xl:grid-cols-4 xl:gap-10 2xl:gap-12">
+            <div v-for="item in tech" :key="item.id">
+              <div
+                class="item-tech flex cursor-pointer items-center gap-2 rounded border border-blue-200 px-2 py-2 hover:bg-blue-200 hover:bg-opacity-10 md:gap-3 lg:px-3">
+                <div class="flex h-12 w-12 items-center justify-center p-0 lg:h-16 lg:w-16 lg:p-2 zoom-in">
+                  <img alt="HTML" loading="lazy" width="32" height="32" decoding="async" data-nimg="1"
+                    class="img-tech drop-shadow-xl transition-all duration-300 h-[65%] w-[65%] lg:h-[85%] lg:w-[85%]  "
+                    :src="item.imageUrl" style="color: transparent;">
+                </div>
+                <div class="flex items-center text-sm md:text-base lg:text-lg">
+                  <div class="tech font-medium text-secondary transition-all duration-300 translate-y-0 ">{{ item.name }}
+                  </div>
+                  <div
+                    class="status-tech opacity-0 absolute mt-5 text-[10px] text-blue-200 transition-all duration-300 md:text-xs lg:text-sm">
+                    {{ item.status }}</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div v-show="activeTab === 2">
+          <div class="grid grid-cols-2 gap-4 pb-32 md:grid-cols-3 md:gap-8 xl:grid-cols-4 xl:gap-10 2xl:gap-12">
+            <div v-for="item in tools" :key="item.id">
+              <div
+                class="item-tech flex cursor-pointer items-center gap-2 rounded border border-blue-200 px-2 py-2 hover:bg-blue-200 hover:bg-opacity-10 md:gap-3 lg:px-3">
+                <div class="flex h-12 w-12 items-center justify-center p-0 lg:h-16 lg:w-16 lg:p-2 zoom-in">
+                  <img alt="HTML" loading="lazy" width="32" height="32" decoding="async" data-nimg="1"
+                    class="img-tech drop-shadow-xl transition-all duration-300 h-[65%] w-[65%] lg:h-[85%] lg:w-[85%]  "
+                    :src="item.imageUrl" style="color: transparent;">
+                </div>
+                <div class="flex items-center text-sm md:text-base lg:text-lg">
+                  <div class="tech font-medium text-secondary transition-all duration-300 translate-y-0 ">{{ item.name }}
+                  </div>
+                  <div
+                    class="status-tech opacity-0 absolute mt-5 text-[10px] text-blue-200 transition-all duration-300 md:text-xs lg:text-sm">
+                    {{ item.status }}</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </article>
+  </div>
+</template>
+
+<style>
+.fadein-left {
+  opacity: 0;
+  animation: fadeInLeft 0.5s ease-out forwards;
+}
+
+@keyframes fadeInLeft {
+  0% {
+    opacity: 0;
+    transform: translateX(100%);
+  }
+
+  100% {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
+
+.fadeins-1 {
+  animation-delay: 500ms;
+}
+
+.fadeins-2 {
+  animation-delay: 800ms;
+}
+
+.img-tech,
+.tech {
+  transition: transform 0.3s ease;
+}
+
+.item-tech:hover .img-tech {
+  transform: scale(1.3);
+}
+
+.item-tech:hover .tech {
+  transform: translateY(-12px);
+}
+
+.item-tech:hover .status-tech {
+  opacity: 1;
+}
+</style>
